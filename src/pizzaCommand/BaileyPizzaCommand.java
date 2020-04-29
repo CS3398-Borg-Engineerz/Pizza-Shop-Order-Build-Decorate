fec1954fba7f9d12362a3c10fb1c8df796af6696
Bailey Cheung <bcheung@openlending.com>
2020-04-29 18:00:55 -0500

    Update BaileyPizzaCommand.java

--- src/pizzaCommand/BaileyPizzaCommand.java
+++ src/pizzaCommand/BaileyPizzaCommand.java
@@ -3,7 +3,7 @@ package patternsexamp;
 import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 
-public class BaileyPizzaCommand {
+public class BaileyPizzaCommand implements PizzaCommand{
   //reference to the order
   PizzaOrder order;
   public BaileyPizzaCommand(PizzaOrder order){
